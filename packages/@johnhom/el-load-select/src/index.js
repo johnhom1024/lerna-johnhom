3fabbc62ad28d5770245b18827df36b24c3c4ea9
anthony <924358746@qq.com>
2021-12-10 00:02:47 +0800

    feature(el-load-select): 更新注释

--- packages/@johnhom/el-load-select/src/index.js
+++ packages/@johnhom/el-load-select/src/index.js
@@ -8,7 +8,9 @@ const Plugin = {
     if (!Vue.prototype.$ELEMENT) {
       throw new Error("请先安装element-ui");
     }
+    // 注册全局组件
     Vue.component(LoadSelect.name, LoadSelect);
+    // 注册指令
     Vue.directive('loadmore', LoadMoreDirective)
     Plugin.installed = true
   }
